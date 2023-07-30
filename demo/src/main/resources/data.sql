61d334baa5db8640923c99402690dc8111d54cf3
abraham <abraham.mathewos.meja@gmail.com>
2023-07-30 16:58:51 -0500

    Remove demo file

--- demo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product (id, name, price, quantity, description, category,color,image) VALUES (1, 'Product 1', 10,10,'desc','cat1', 'red','image1');
-INSERT INTO product (id, name, price, quantity, description, category,color,image) VALUES (2, 'Product 2', 1,10,'desc','cat1', 'red','image1');
-INSERT INTO product (id, name, price, quantity, description, category,color,image) VALUES (3, 'Product 3', 2,10,'desc','cat1', 'red','image1');
