bdb99cf24235e9e23eba7a0e8723088004cf6b8f
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-30 07:56:43 -0500

    remove comment

--- project/src/main/java/com/waa/project/util/LoggingUtil.java
+++ project/src/main/java/com/waa/project/util/LoggingUtil.java
@@ -4,12 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class LoggingUtil {
-    // Private constructor to prevent instantiation of the utility class
     public LoggingUtil() {
 
     }
 
-    // Example method that uses the logger to log a message
     public static void logMessage(String message) {
         String callerClassName = new Throwable().getStackTrace()[1].getClassName();
         Logger log = LoggerFactory.getLogger(callerClassName);
