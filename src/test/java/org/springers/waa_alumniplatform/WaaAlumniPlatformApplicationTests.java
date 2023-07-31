7095321d9443781a8a1e830a54f06a3e18f5d05d
Haileab-TG <haileab.t.g@gmail.com>
2023-07-31 12:11:28 -0500

    initial commit

---
+++ src/test/java/org/springers/waa_alumniplatform/WaaAlumniPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springers.waa_alumniplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaAlumniPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
