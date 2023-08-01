b275ecc7c6e404c5a6626c764b6617a0f3c6b45f
YoditAbraha <107884901+YoditAbraha@users.noreply.github.com>
2023-08-01 18:58:42 -0500

    update job posting

--- project/src/main/java/com/waa/project/controller/JobAdvertisementController.java
+++ project/src/main/java/com/waa/project/controller/JobAdvertisementController.java
@@ -1,8 +1,11 @@
 package com.waa.project.controller;
 
 import com.waa.project.entity.JobAdvertisement;
+import com.waa.project.entity.UserRole;
+import com.waa.project.service.AuthenticationService;
 import com.waa.project.service.JobAdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,19 +15,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/jobAdvertisements")
 public class JobAdvertisementController {
 
+    private final AuthenticationService authenticationService;
+
     private final JobAdvertisementService jobAdvertisementService;
 
     @Autowired
-    public JobAdvertisementController(JobAdvertisementService jobAdvertisementService) {
+    public JobAdvertisementController(AuthenticationService authenticationService, JobAdvertisementService jobAdvertisementService) {
+        this.authenticationService = authenticationService;
         this.jobAdvertisementService = jobAdvertisementService;
     }
 
+    @PostMapping
+    public ResponseEntity<JobAdvertisement> createJobAdvertisement(@RequestBody JobAdvertisement jobAdvertisement) {
+
+        JobAdvertisement createdJobAdvertisement = jobAdvertisementService.save(jobAdvertisement);
+
+        if (createdJobAdvertisement != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdJobAdvertisement);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
     @PreAuthorize("hasRole('ALUMNI') and #jobAdvertisement.student.email == authentication.principal.username")
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateJobAdvertisement(
-            @PathVariable long id,
-            @RequestBody JobAdvertisement jobAdvertisement
-    ) {
+    @PutMapping
+    public ResponseEntity<String> updateJobAdvertisement(@RequestBody JobAdvertisement jobAdvertisement) {
         String loggedInUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         return jobAdvertisementService.updateJobAdvertisement(jobAdvertisement, loggedInUserEmail);
     }
