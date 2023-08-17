759ede79f54cff333aa1a8d28860f3d18e6b3fc3
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-08-17 17:43:55 -0500

    update system log api

--- backend/src/main/java/com/waa/project/controller/AdminController.java
+++ backend/src/main/java/com/waa/project/controller/AdminController.java
@@ -1,20 +1,25 @@
 package com.waa.project.controller;
 
-import com.waa.project.aspect.annotation.CheckUserActive;
 import com.waa.project.aspect.annotation.LogMe;
 import com.waa.project.dto.requestDto.NewPasswordDto;
+import com.waa.project.dto.requestDto.SystemLogRequestDto;
+import com.waa.project.dto.responseDto.CustomResponseDto;
 import com.waa.project.service.AuthenticationService;
+import com.waa.project.service.SystemLogService;
 import com.waa.project.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
     private final UserService userService;
+    private final SystemLogService systemLogService;
     private final AuthenticationService authenticationService;
     @LogMe
     @PostMapping("/activate/{userId}")
@@ -33,5 +38,17 @@ public class AdminController {
     public ResponseEntity<?> resetPassword(@PathVariable Long userId, @RequestBody NewPasswordDto newPasswordDto) {
         userService.resetUserPassword(userId, newPasswordDto);
         return ResponseEntity.ok("Password reset successfully");
+    }
+    @LogMe
+    @GetMapping("/logs")
+    public ResponseEntity<CustomResponseDto<List<SystemLogRequestDto>>> getAllLogs() {
+        List<SystemLogRequestDto> logs = systemLogService.getSystemLogs();
+        if(logs.isEmpty()) {
+            CustomResponseDto<List<SystemLogRequestDto>> response = new CustomResponseDto<>("204","No Data",null);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+        } else {
+            CustomResponseDto<List<SystemLogRequestDto>> response = new CustomResponseDto<>("200","Found Data",logs);
+            return ResponseEntity.ok().body(response);
+        }
     }
 }
